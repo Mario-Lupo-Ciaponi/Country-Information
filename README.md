979844b82eaa9197b478ee0a11d1b15cc161d467
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2024-02-06 14:06:14 +0200

    Added the python file and update readme file.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Country-Information
-This is a console app in which you can type a country an you will get information about it.
+This is a console app in which you can type a country and you will get information about it. This program uses the
+library "countryinfo".
+
+## Information give:
+- Language/s;
+- Capital city;
+- Population;
+- Currency/ies
+- Native's name
+- Resident's name;
+- Area(km²);
+- Province
+- Region;
+- Borders;
+- Calling code;
+- Time zone;
+- Domain;
+- More information(link to wikipedia).
+
+## Purpose:
+To practice API and GitHub skills and to increase my portfolio.
---
+++ country_information.py
@@ -0,0 +1,44 @@
+from countryinfo import CountryInfo
+
+
+def getting_country_information(country_name):
+    """
+      This function returns the information of a country.
+    """
+
+    return CountryInfo(country_name)
+
+
+def showing_country_information(country_information, country_name):
+    """
+      This function prints the information about the given country.
+    """
+    print(f"\nInformation about {country_name}:\n")
+
+    print(f"Language/s: {", ".join(country_information.languages())}")
+    print(f"Capital city: {"".join(country_information.capital())}")
+    print(f"Population: {country_information.population()}")
+    print(f"Currency/ies: {"".join(country_information.currencies())}")
+    print(f"Native's name: {country_information.native_name()}")
+    print(f"Resident's name: {country_information.demonym()}")
+    print(f"Area: {country_information.area()} km²")
+    print(f"Province: {", ".join(country_information.provinces())}")
+    print(f"Region: {country_information.subregion()}")
+    print(f"Borders: {", ".join(country_information.borders())}")
+    print(f"Calling code: +{", ".join(country_information.calling_codes())}")
+    print(f"Time zone: {", ".join(country_information.timezones())}")
+    print(f"Domain: {", ".join(country_information.tld())}")
+    print(f"More information: {country_information.wiki()}")
+
+
+def main():
+    """
+     This is the main function in which you give the name of the country.
+    """
+    country_name = input("Type the name of the country that you want to see the information: ")
+    country_information = getting_country_information(country_name)
+
+    showing_country_information(country_information, country_name)
+
+
+main()
